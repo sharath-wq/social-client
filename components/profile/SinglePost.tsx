5ac699761655a012a57f17a2e73a3f63335c8be7
Sharath-wq <chandranpsharath@gmail.com>
2024-02-20 19:19:00 +0530

    delete post on the profile page

--- components/profile/SinglePost.tsx
+++ components/profile/SinglePost.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from '@/components/ui/context-menu';
 
-const SinglePost = ({ imageUrl }: { imageUrl: string }) => {
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import useRequest from '@/hooks/useRequest';
+import { toast } from '../ui/use-toast';
+
+const SinglePost = ({ imageUrl, id, fetchUserPosts }: { imageUrl: string; id: string; fetchUserPosts: () => void }) => {
+    const handleDelete = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        doRequest();
+    };
+
+    const { doRequest, errors } = useRequest({
+        url: `/api/posts/${id}`,
+        method: 'delete',
+        body: {},
+        onSuccess: () => {
+            toast({
+                description: 'Post Deleted',
+            });
+            fetchUserPosts();
+        },
+    });
+
     return (
-        <div className='p-4 rounded-md'>
-            <img src={imageUrl} alt='Post' className='w-full h-60 object-contain mb-4 rounded-md' />
-        </div>
+        <ContextMenu>
+            <ContextMenuTrigger>
+                <div className='p-4 rounded-md'>
+                    <img src={imageUrl} alt='Post' className='w-full h-60 object-contain mb-4 rounded-md' />
+                </div>
+            </ContextMenuTrigger>
+            <ContextMenuContent className='w-64'>
+                <ContextMenuItem inset>
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <span onClick={(e) => e.stopPropagation()} className='text-red-500'>
+                                Delete
+                            </span>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action cannot be undone. This will permanently delete your post.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction onClick={handleDelete}>Confirm</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </ContextMenuItem>
+                <ContextMenuItem inset>Share</ContextMenuItem>
+            </ContextMenuContent>
+        </ContextMenu>
     );
 };
 
