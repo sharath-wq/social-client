11e84808b5e813973073b136fa294be881c87f9e
Sharath-wq <chandranpsharath@gmail.com>
2024-03-20 17:29:44 +0530

    post reports

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+export const reasonsToReport = [
+    'Hate speech or discriminatory language',
+    'Violent or threatening content',
+    'Harassment or bullying',
+    'Graphic violence or gore',
+    'Sexual content or nudity',
+    'Impersonation or identity theft',
+    'Spam or deceptive content',
+    'Misinformation or fake news',
+    'Intellectual property infringement',
+    'Self-harm or suicide promotion',
+    'Animal abuse or cruelty',
+    'Child exploitation or abuse',
+    "I just don't like it",
+];
